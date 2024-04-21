9730bb988314e0cdbb07f8b8691dfb662164dfef
Mohamed Mahdy <technology@debentms.com>
2024-04-21 12:12:47 +0300

    change

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -31,6 +31,7 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
